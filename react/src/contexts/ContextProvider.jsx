d66d62883328db98e04df69d11a78a2d82580630
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-04-09 23:39:15 -0400

    rétablir le token à son état dans localStorage

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -11,9 +11,9 @@ const StateContext = createContext({
 // Créer un composant fournisseur de contexte pour gérer et fournir l'authentification
 export const ContextProvider = ({children}) => {
   const [user, setUser] = useState({});
-  // const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+  const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
   // Utiliser la ligne suivante pour accéder aux vues protégées
-  const [token, _setToken] = useState(123);
+  // const [token, _setToken] = useState(123);
 
   const setToken = (token) => {
     _setToken(token)
