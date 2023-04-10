bff0965b26e61692da99b5ce5500e974fc40a0db
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-04-09 22:15:33 -0400

    importation de react dans le fichier main me semble encore nécéssaire

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,5 +1,5 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
@@ -10,5 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ContextProvider>
       <RouterProvider router={router} />
     </ContextProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
