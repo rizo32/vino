bec6558f18c425d2ca3794016a196c03678c9db1
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-04-19 20:34:51 -0400

    modifications du pannel admin -ajout d'un boutons liés a fetch produit --YG

--- react/src/views/Admin.jsx
+++ react/src/views/Admin.jsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const baseURL = "http://localhost:8000/api/saq/getProduits";
+const baseURL = "http://localhost:8000/api/saq";
 
 const Admin = () => {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await axios.get(`${baseURL}/24/1`);
-      setProducts(response.data);
-    };
-
-    fetchProducts();
-  }, []);
+  const fetchProducts = async () => {
+    const response = await axios.post(`${baseURL}/fetch`, {
+      nombre: 24,
+      page: 1,
+    });
+    setProducts(response.data);
+  };
 
   return (
     <div>
       <h1>Admin</h1>
+      <button onClick={fetchProducts}>Fetch Products</button>
       {products.map((product, index) => (
         <div key={index}>
           <h2>{product.nom}</h2>
