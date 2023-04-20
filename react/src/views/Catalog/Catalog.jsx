01ca7586c545a8fd9a3bce4c8d329cda8b607f19
nicolasgodineau <nicolasgodineau@live.fr>
2023-04-20 08:44:17 -0400

    Acceptation des modifications entrantes

--- react/src/views/Catalog/Catalog.jsx
+++ react/src/views/Catalog/Catalog.jsx
@@ -5,36 +5,50 @@ import img from "./img/white-wine.webp";
 import "./style/catalog.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import axiosClient from "../../axios-client";
->>>>>>> upstream/dev
 
 export default function Catalog() {
     const [bottles, setBottles] = useState([]);
     const [loading, setLoading] = useState(true);
 
-<<<<<<< HEAD
-    const getBottles = async () => {
-        try {
-            const response = await axios.get(
-                "http://127.0.0.1:8000/api/bottles"
-            );
-            console.log(response);
-            return response.data;
-        } catch (error) {
-            console.error(error);
-        }
+    // const getBottles = async () => {
+    //   try {
+    //     const response = await axios.get("http://127.0.0.1:8000/api/bottles");
+    //     console.log(response);
+    //     return response.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+
+    // useEffect(() => {
+    //   const fetchBottles = async () => {
+    //     const data = await getBottles();
+    //     console.log(data);
+    //     setBottles(data.data);
+    //     setLoading(false);
+    //   };
+
+    //   fetchBottles();
+    // }, []);
+
+    const getBottles = () => {
+        setLoading(true);
+        axiosClient
+            .get("/bottles")
+            .then(({ data }) => {
+                console.log(data);
+                setBottles(data.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error(error);
+                setLoading(false);
+            });
     };
 
     useEffect(() => {
-        const fetchBottles = async () => {
-            const data = await getBottles();
-            setBottles(data.data);
-            setLoading(false);
-        };
-
-        fetchBottles();
+        getBottles();
     }, []);
 
     return (
@@ -55,82 +69,13 @@ export default function Catalog() {
                     {bottles.map((bottle) => (
                         // <li key={bottle.id}>{bottle.name} - {bottle.description}</li>
                         <li key={bottle.id}>
-                            <Link to={`/product/${bottle.id}`}>
-                                <ProductCard bottle={bottle} />
-                            </Link>
+                            {/* <Link to={`/product/${bottle.id}`}> */}
+                            <ProductCard bottle={bottle} />
+                            {/* </Link> */}
                         </li>
                     ))}
                 </ul>
             )}
         </div>
     );
-=======
-  // const getBottles = async () => {
-  //   try {
-  //     const response = await axios.get("http://127.0.0.1:8000/api/bottles");
-  //     console.log(response);
-  //     return response.data;
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const fetchBottles = async () => {
-  //     const data = await getBottles();
-  //     console.log(data);
-  //     setBottles(data.data);
-  //     setLoading(false);
-  //   };
-
-  //   fetchBottles();
-  // }, []);
-  
-  const getBottles = () => {
-    setLoading(true);
-    axiosClient
-      .get("/bottles")
-      .then(({ data }) => {
-        console.log(data);
-        setBottles(data.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        setLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    getBottles();
-  }, []);
-
-  return (
-    <div className="flex flex-col gap-2">
-      <TextOnImage
-        text="Vins blancs"
-        imagePath={img}
-        alt="vins blancs"
-        objectTop="object-top-20"
-        contrast="contrast-120"
-        saturation="saturation-70"
-        brightness="brightness-90"
-      />
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {bottles.map((bottle) => (
-            // <li key={bottle.id}>{bottle.name} - {bottle.description}</li>
-            <li key={bottle.id}>
-              {/* <Link to={`/product/${bottle.id}`}> */}
-              <ProductCard bottle={bottle} />
-              {/* </Link> */}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
->>>>>>> upstream/dev
 }
