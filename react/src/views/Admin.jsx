8a0cec5d1de258960a4c1fe84952405422a38e79
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-04-19 20:41:32 -0400

    ajout esthetique temporaire du bouton fetchProduits, adminpanel --YG

--- react/src/views/Admin.jsx
+++ react/src/views/Admin.jsx
@@ -15,9 +15,14 @@ const Admin = () => {
   };
 
   return (
-    <div>
-      <h1>Admin</h1>
-      <button onClick={fetchProducts}>Fetch Products</button>
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-semibold mb-4">Admin</h1>
+      <button
+        onClick={fetchProducts}
+        className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 mb-4"
+      >
+        Fetch Products
+      </button>
       {products.map((product, index) => (
         <div key={index}>
           <h2>{product.nom}</h2>
