9def3601c79d7c2daff9c8ccdc96b7dee9f04f5e
rizo32 <g.rizzoli@hotmail.ca>
2023-04-20 03:46:10 -0400

    ajout de la relation cellarHasBottles dans Bottle

--- app/Models/Bottle.php
+++ app/Models/Bottle.php
@@ -7,39 +7,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bottle extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $table = 'bottles';
-    protected $primaryKey = 'id';
-    protected $fillable = [
-        'name',
-        'image_path',
-        'code_saq',
-        'country_id',
-        'description',
-        'price_saq',
-        'url_saq',
-        'image_url',
-        'format_id',
-        'type_id',
-        'milesime',
-        'rating_saq',
-    ];
+  protected $table = 'bottles';
+  protected $primaryKey = 'id';
+  protected $fillable = [
+    'name',
+    'image_path',
+    'code_saq',
+    'country_id',
+    'description',
+    'price_saq',
+    'url_saq',
+    'image_url',
+    'format_id',
+    'type_id',
+    'milesime',
+    'rating_saq',
+  ];
 
-    public function format()
-    {
-        return $this->belongsTo(Format::class);
-    }
+  public function format()
+  {
+    return $this->belongsTo(Format::class);
+  }
 
-    public function country()
-    {
-        return $this->belongsTo(Country::class);
-    }
+  public function country()
+  {
+    return $this->belongsTo(Country::class);
+  }
 
-    public function type()
-    {
-        return $this->belongsTo(Type::class);
-    }
+  public function type()
+  {
+    return $this->belongsTo(Type::class);
+  }
+
+  public function cellarHasBottles()
+  {
+    return $this->hasMany(CellarHasBottle::class);
+  }
 
 
 }
