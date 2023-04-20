a593659af5993b1288bbdc5402397e58f28295f5
rizo32 <g.rizzoli@hotmail.ca>
2023-04-20 10:17:02 -0400

    fix signup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('profil_picture_path',255);
+            $table->string('profil_picture_path',255)->nullable();
             $table->timestamps();
         });
     }
