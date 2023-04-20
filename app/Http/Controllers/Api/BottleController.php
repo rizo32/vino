4e2c335fd6babdd6b56c9412a691184802468596
rizo32 <g.rizzoli@hotmail.ca>
2023-04-20 03:43:50 -0400

    méthodes cellar has bottle

--- app/Http/Controllers/Api/BottleController.php
+++ app/Http/Controllers/Api/BottleController.php
@@ -19,7 +19,9 @@ class BottleController extends Controller
      */
     public function index()
     {
-        return BottleResource::collection(Bottle::query()->orderBy('id', 'desc')->paginate(10));
+        return BottleResource::collection(
+          Bottle::query()->orderBy('id', 'desc')->paginate(10)
+        );
     }
 
     /**
