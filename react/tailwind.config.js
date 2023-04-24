9cd3daddc5a1911375cc2a85ceab6e2e285f4e74
rizo32 <g.rizzoli@hotmail.ca>
2023-04-24 19:02:36 -0400

    style + message de validation login

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -8,6 +8,10 @@ export default {
           },
           boxShadow: {
             'shadow-tiny': '0 1px 1px 0 rgba(0, 0, 0, 0.25)',
+            'shadow-tiny-inset': 'inset 0 1px 1px 0 rgba(0, 0, 0, 0.25)',
+          },
+          margin: {
+            "vh-10": "10vh",
           },
         },
     },
