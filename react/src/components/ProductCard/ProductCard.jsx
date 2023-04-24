1ef5ae3f3af28e588a45a7075d4d2dc116414441
rizo32 <g.rizzoli@hotmail.ca>
2023-04-24 14:51:55 -0400

    diminution taille de la composante productCard

--- react/src/components/ProductCard/ProductCard.jsx
+++ react/src/components/ProductCard/ProductCard.jsx
@@ -7,14 +7,14 @@ import bouteille1 from "./img/bouteille1.webp";
 import axiosClient from "../../axios-client";
 
 export default function ProductCard({ bottle }) {
-
   // fonction pour ajouter une bouteille au cellier
   const addToCellar = (bottle) => {
-    axiosClient.post(`${import.meta.env.VITE_API_BASE_URL}/api/cellarHasBottles`, bottle)
-    .catch(err => {
-      console.log(err.response);
-    })
-  }
+    axiosClient
+      .post(`${import.meta.env.VITE_API_BASE_URL}/api/cellarHasBottles`, bottle)
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
 
   return (
     // Il faudra mettre le lien en évitant que d'appuyer sur le "+" ou swiper
@@ -22,36 +22,21 @@ export default function ProductCard({ bottle }) {
     /* <Link to={`/product/${bottle.id}`}> */
     <article
       id="ProductCard"
-      className="flex flex-row items-center justify-center py-6 mb-2 bg-white"
+      className="flex flex-row justify-center py-4 mb-2 bg-white"
     >
-      <img className="h-40" src={bottle.image_url} alt="" />
-      <section className="flex-flex_column justify-start gap-3 bg-white">
-        <div className="flex ">
-          <h2 className="font-bold">{bottle.name}</h2>
-          <div className="px-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="w-12 h-12"
-              onClick={() => addToCellar(bottle)}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 6v12m6-6H6"
-              />
-            </svg>
-          </div>
+      <div className="flex flex-col justify-center">
+        <img className="h-36 object-contain" src={bottle.image_url} alt="" />
+      </div>
+      <section className="flex flex-col justify-start items-start gap-3 bg-white flex-grow">
+        <h2 className="font-bold">{bottle.name}</h2>
+        <div className="flex gap-3">
+          <p className="font-light">{bottle.type}</p> <span>|</span> <p className="font-light">{bottle.format}</p>
         </div>
+        <p className="font-light">
+          {bottle.country}, {bottle.state}
+        </p>
         <div className="flex gap-4">
-          <p>{bottle.type}</p> <span>|</span> <p>{bottle.format}</p>
-        </div>
-        <p>{bottle.country}</p>
-        <div className="flex gap-4">
-          <p className="flex">
+          <span className="flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -107,12 +92,28 @@ export default function ProductCard({ bottle }) {
                 d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
               />
             </svg>
-          </p>
-          
+          </span>
           {/* Propriété à venir?: {bottle.numberOfReview????} */}
-          <span>|</span> <p>? avis</p>
+          <span>|</span> <p className="font-light">? avis</p>
         </div>
       </section>
+      <div className="px-4 py-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-12 h-12"
+          onClick={() => addToCellar(bottle)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6v12m6-6H6"
+          />
+        </svg>
+      </div>
     </article>
     /* </Link> */
   );
