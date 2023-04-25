15c29c459faaa9e06e06ba42976591cc5bed6bd2
rizo32 <g.rizzoli@hotmail.ca>
2023-04-25 01:52:33 -0400

    ajout du titre du site

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,16 +1,21 @@
 import { Navigate, Outlet, Link } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 import MobileNavbar from "../components/MobileNavbar/MobileNavbar";
+import { Helmet } from "react-helmet";
 
 export default function GuestLayout() {
-    const { token } = useStateContext();
+  const { token } = useStateContext();
 
   // si l'utilisateur est connecté, il n'accèdera pas aux pages enfants de GuestLayout (login, signup)
   if (token) {
     return <Navigate to="/" />;
   }
   return (
+    // Gab: pas de barre de navigation dans Guest Layout. Faut utiliser 'login' et 'signup'
     <div id="GuestLayout">
+      <Helmet>
+        <title>Le Cellier</title>
+      </Helmet>
       <main className="pt-16 bg-red-50 min-h-screen">
         {/* Outlet va aller chercher la vue appropriée dans le router */}
         <Outlet />
