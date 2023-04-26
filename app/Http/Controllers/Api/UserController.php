d00db07cc950889f8f1d3390bab4fb7cb880fe31
rizo32 <g.rizzoli@hotmail.ca>
2023-04-26 01:03:53 -0400

    ajout de la méthode User::update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UpdateUserRequest;
 
 class UserController extends Controller
 {
@@ -26,7 +28,7 @@ class UserController extends Controller
    */
   public function show($id)
   {
-    $user = User::findOrFail($id);
+    $user = User::findOrFail($id); // Trouve le user par id, sinon envois 404
     return response()->json($user);
   }
 
@@ -37,10 +39,23 @@ class UserController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id)
+  public function update(UpdateUserRequest $request, $id)
   {
-    //
+    $data = $request->validated();
+
+    /** @var \App\Models\User $user */
+    $user = User::findOrFail($id); // Trouve le user par id, sinon envois 404
+
+    $user->update([
+      'first_name' => $data['first_name'],
+      'last_name' => $data['last_name'],
+      'email' => $data['email'],
+      'password' => bcrypt($data['password']),
+    ]);
+
+    return response(compact('user'));
   }
+
 
   /**
    * Remove the specified resource from storage.
