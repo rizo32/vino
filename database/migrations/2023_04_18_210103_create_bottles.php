eb69515b92d4ac45f5e58bedf3b805c5d1c7e43c
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-04-27 14:54:07 -0400

    retrait de colonnes desuet --YG

--- database/migrations/2023_04_18_210103_create_bottles.php
+++ database/migrations/2023_04_18_210103_create_bottles.php
@@ -19,15 +19,13 @@ return new class extends Migration {
       $table->id();
       $table->string('name', 90);
       $table->string('code_saq', 255);
-      $table->text('description');
       $table->decimal('price_saq', 8, 2);
       $table->string('image_url', 255)->nullable();
       $table->string('url_saq', 255)->nullable();
       $table->foreignId('format_id')->constrained()->onDelete('cascade')->nullable();
       $table->foreignId('country_id')->constrained()->onDelete('cascade')->nullable();
       $table->foreignId('type_id')->constrained()->onDelete('cascade')->nullable();
-      $table->integer('millesime')->nullable();
-      $table->string('rating_saq')->nullable();
+      $table->float('rating_saq')->nullable();
       $table->timestamps();
   });
   
