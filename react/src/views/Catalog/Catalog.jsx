a6755101fb5b7aeb538d7f66a6dac1e4af37ace5
Élodie <83143421+88elodie@users.noreply.github.com>
2023-05-03 10:47:25 -0400

    fix pagination with search and filters and no fetch loop

--- react/src/views/Catalog/Catalog.jsx
+++ react/src/views/Catalog/Catalog.jsx
@@ -8,8 +8,8 @@ export default function Catalog() {
     const { searchValue } = useStateContext();
     const [bottles, setBottles] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [meta, setMeta] = useState();
-    const [links, setLinks] = useState();
+    const [onPage, setOnPage] = useState();
+    const [total, setTotal] = useState();
     const [scrollPosition, setScrollPosition] = useState(0);
     const [page, setPage] = useState(1);
     const containerRef = useRef(null);
@@ -21,11 +21,15 @@ export default function Catalog() {
         ratings: [],
     });
 
+    const [oldFilters, setOldFilters] = useState();
+    const [oldSearch, setOldSearch] = useState();
+
     // Elodie
     // aller chercher les bouteilles dans la base de données et les mettre dans le state
     const getBottles = (bottleUpdt) => {
         setLoading(true); // à mettre en place (eg Gif)
         setScrollPosition(window.pageYOffset);
+
         const filterParams = new URLSearchParams();
 
         // Change la requête selon recherche/filtre
@@ -46,18 +50,19 @@ export default function Catalog() {
         if(bottleUpdt){
             const updatedBottles = bottles.map(bottle => {
                 if (bottle.id === bottleUpdt.id && !bottle.quantity) {
-                  // Create a new object with the same properties as the current `bottle`, but with the additional `newProperty`
+                  // ajouter la propriete quantite sans recharcher toutes les bouteilles
                   return {
                     ...bottle,
                     quantity: 1
                   };
                 }else if (bottle.id === bottleUpdt.id && bottle.quantity) {
+                    //augmenter la quantite si elle existe
                     return {
                         ...bottle,
                         quantity: bottle.quantity + 1
                       };
                 }
-                // If the `bottle` is not the one we want to modify, return the original `bottle` object
+                // garder meme bouteille et proprietes si rien change
                 return bottle;
             });
             setBottles(updatedBottles);
@@ -65,14 +70,25 @@ export default function Catalog() {
             return;
         }
 
+        if(oldFilters != filters || oldSearch != searchValue){
+            setPage(1);
+        }else{
+            setPage(page + 1)
+        }
+
         axiosClient
             .get(`/bottles?${filterParams.toString()}&page=${page}`)
             .then(({ data }) => {
-                setBottles([...bottles, ...data.data]);
-                setMeta(data.meta);
-                setLinks(data.links);
-                setPage(data.meta.current_page + 1);
+                if(page == 1){
+                    setBottles(data.data)
+                }else{
+                    setBottles([...bottles, ...data.data]);
+                }
+                setOnPage(data.meta.to);
+                setTotal(data.meta.total);
                 setLoading(false);
+                setOldFilters(filters);
+                setOldSearch(searchValue);
             })
             .catch((error) => {
                 console.error(error.response);
@@ -82,7 +98,7 @@ export default function Catalog() {
 
     //lorsque le sentinel entre en vue, charger la prochaine page
     const handleIntersection = (entries) => {
-        if (entries[0].isIntersecting) {
+        if (entries[0].isIntersecting && onPage == 10 && total > 10) {
             getBottles();
         }
     };
@@ -126,7 +142,7 @@ export default function Catalog() {
                             />
                         </li>
                     ))}
-                    <div ref={(el) => (sentinelRef.current = el)}>test</div>
+                    <div ref={(el) => (sentinelRef.current = el)} id="sentinel">test</div>
                 </ul>
             )}
         </div>
