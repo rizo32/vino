6b0cc111de3aed85d735e4f34be356c104d76a3f
rizo32 <g.rizzoli@hotmail.ca>
2023-05-03 11:45:04 -0400

    Enlever les produits par défaut et filtre

--- react/src/views/Catalog/Catalog.jsx
+++ react/src/views/Catalog/Catalog.jsx
@@ -14,6 +14,7 @@ export default function Catalog() {
     const [page, setPage] = useState(1);
     const containerRef = useRef(null);
     const sentinelRef = useRef();
+    const [hasMoreResults, setHasMoreResults] = useState(true);
 
     const [filters, setFilters] = useState({
         type: [],
@@ -28,34 +29,34 @@ export default function Catalog() {
         setScrollPosition(window.pageYOffset);
         const filterParams = new URLSearchParams();
 
-        // Change la requête selon recherche/filtre
+        // Change la requête selon filtre country
         if (filters.country.length > 0) {
             filterParams.append("country", filters.country.join(","));
         }
 
-        // Change la requête selon recherche/filtre
+        // Change la requête selon filtre type
         if (filters.type.length > 0) {
             filterParams.append("type", filters.type.join(","));
         }
 
+        // Change la requête selon recherche
         if (searchValue) {
             filterParams.append("search", searchValue);
         }
-        // autres filtres
 
-        if(bottleUpdt){
-            const updatedBottles = bottles.map(bottle => {
+        if (bottleUpdt) {
+            const updatedBottles = bottles.map((bottle) => {
                 if (bottle.id === bottleUpdt.id && !bottle.quantity) {
-                  // Create a new object with the same properties as the current `bottle`, but with the additional `newProperty`
-                  return {
-                    ...bottle,
-                    quantity: 1
-                  };
-                }else if (bottle.id === bottleUpdt.id && bottle.quantity) {
+                    // Create a new object with the same properties as the current `bottle`, but with the additional `newProperty`
                     return {
                         ...bottle,
-                        quantity: bottle.quantity + 1
-                      };
+                        quantity: 1,
+                    };
+                } else if (bottle.id === bottleUpdt.id && bottle.quantity) {
+                    return {
+                        ...bottle,
+                        quantity: bottle.quantity + 1,
+                    };
                 }
                 // If the `bottle` is not the one we want to modify, return the original `bottle` object
                 return bottle;
@@ -65,6 +66,9 @@ export default function Catalog() {
             return;
         }
 
+        // GAB DEBUG
+        console.log("Current page:", page);
+
         axiosClient
             .get(`/bottles?${filterParams.toString()}&page=${page}`)
             .then(({ data }) => {
@@ -72,6 +76,18 @@ export default function Catalog() {
                 setMeta(data.meta);
                 setLinks(data.links);
                 setPage(data.meta.current_page + 1);
+
+                // GAB DEBUG
+                if (data.meta.current_page < data.meta.last_page) {
+                    setPage(data.meta.current_page + 1);
+                    setHasMoreResults(true);
+                } else {
+                    setHasMoreResults(false);
+                }
+                setPage(data.meta.current_page + 1);
+                setHasMoreResults(data.meta.current_page < data.meta.last_page);
+                console.log('Current page:', data.meta);
+
                 setLoading(false);
             })
             .catch((error) => {
@@ -83,13 +99,21 @@ export default function Catalog() {
     //lorsque le sentinel entre en vue, charger la prochaine page
     const handleIntersection = (entries) => {
         if (entries[0].isIntersecting) {
+            // if (entries[0].isIntersecting && hasMoreResults) {
             getBottles();
         }
     };
 
-    // executer fonction
+    // Fetch bouteille seulement lors de la recherche
     useEffect(() => {
+        if ((searchValue || filters.type.length > 0 || filters.country.length > 0) && hasMoreResults) {
+
+            setPage(1); // Devrait reset la page à 1 à chaque changement de searchValue mais ne semble pas fonctionner...
+            console.log('page ', page);
         getBottles();
+        } else {
+            setBottles([]);
+        }
     }, [filters, searchValue]);
 
     //sentinel observer pour la pagination scroll
@@ -113,22 +137,22 @@ export default function Catalog() {
 
     return (
         <div className="flex flex-col gap-2 mb-[100px]" ref={containerRef}>
-            <FilterPanel filters={filters} setFilters={setFilters} />
-            {loading ? (
+            {/* Désactivation du filtre dans le catalogue avant l'implantation d'une liste d'achat qui justifierait une recherche plus appronfondie */}
+            {/* <FilterPanel filters={filters} setFilters={setFilters} /> */}
+
+            {/* Loading state n'est pas nécéssaire dans l'état actuel des choses mais pourrait le devenir */}
+            {/* {loading ? (
                 <p>Chargement...</p>
-            ) : (
-                <ul className="flex flex-col gap-2">
-                    {bottles.map((bottle) => (
-                        <li key={bottle.id}>
-                            <ProductCard
-                                bottle={bottle}
-                                getBottles={getBottles}
-                            />
-                        </li>
-                    ))}
-                    <div ref={(el) => (sentinelRef.current = el)}>test</div>
-                </ul>
-            )}
+            ) : ( */}
+            <ul className="flex flex-col gap-2">
+                {bottles.map((bottle) => (
+                    <li key={bottle.id}>
+                        <ProductCard bottle={bottle} getBottles={getBottles} />
+                    </li>
+                ))}
+                {/* <div ref={(el) => (sentinelRef.current = el)}>test</div> */}
+            </ul>
+            {/* )} */}
         </div>
     );
 }
