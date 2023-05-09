a255d42fb5da11beb22ee47f66a7dbe78360c8dc
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-09 13:14:20 -0400

    suppression de colonnes obsoletes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->string('profil_picture_path',255)->nullable();
             $table->unsignedBigInteger('user_type_id')->nullable();
             $table->foreign('user_type_id')->references('id')->on('user_types')->onDelete('cascade');
             $table->timestamps();
