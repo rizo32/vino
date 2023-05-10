1491df29a6104f6252d1befd853819065f99f99f
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-10 10:46:52 -0400

    ajout de modal , version temp --YG

--- react/src/views/Admin.jsx
+++ react/src/views/Admin.jsx
@@ -6,6 +6,8 @@ const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/admin`;
 const Admin = () => {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
 
   useEffect(() => {
     axios
@@ -26,6 +28,16 @@ const Admin = () => {
     user.first_name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const openModal = (user) => {
+    setSelectedUser(user);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedUser(null);
+  };
+
   return (
     <div className="pt-4 container mx-auto">
       <h1 className="text-2xl flex justify-center font-bold mb-4">Modifications utilisateurs</h1>
@@ -40,14 +52,37 @@ const Admin = () => {
       </div>
       <div className="grid grid-cols-2 gap-4">
         {filteredUsers.map((user) => (
-          <div key={user.id} className="bg-gray-200 p-4 rounded-lg shadow">
-            <p className="text-lg font-bold">{user.first_name}</p>
-            <p>{user.email}</p>
+          <div key={user.id} className="bg-gray-200 p-4 rounded-lg shadow flex items-center justify-between">
+            <div>
+              <p className="text-lg font-bold">{user.first_name}</p>
+              <p>{user.email}</p>
+            </div>
+            <button className="px-4 py-2 bg-red-900 text-white rounded-lg" onClick={() => openModal(user)}>
+              Modifications
+            </button>
           </div>
         ))}
       </div>
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white p-8 max-w-md mx-auto rounded-lg shadow-lg">
+            <h2 className="text-xl font-bold mb-4">Modifications utilisateur</h2>
+            {selectedUser && (
+              <div>
+                <p className="text-lg">User: {selectedUser.first_name}</p>
+                <p>Email: {selectedUser.email}</p>
+              </div>
+            )}
+            <button className="px-4 py-2 bg-red-900 text-white rounded-lg mt-4" onClick={closeModal}>
+              Fermer
+            </button>
+            
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Admin;
+
