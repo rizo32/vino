ebc43576c39b4383163080d826aabe9abdf24edd
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-10 15:58:34 -0400

    modifications du types de données a recevoir data1

--- react/src/components/Stats/premierDataSet.jsx
+++ react/src/components/Stats/premierDataSet.jsx
@@ -40,21 +40,21 @@ export const data = {
   labels,
   datasets: [
     {
-      label: `nombre d'utilisateurs`,
+      label: `nombre total d'utilisateurs`,
       data: [450, 700, 320, 580, 820],
       borderColor: 'rgba(255, 99, 132, 1)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
       fill: false,
     },
     {
-      label: `nombre de bouteilles ajouter`,
+      label: `moyennes des bouteilles ajouter / utilisateur`,
       data: [280, 540, 760, 390, 620],
       borderColor: 'rgba(53, 162, 235, 1)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
       fill: false,
     },
     {
-        label: `nombre de bouteilles supprimer`,
+        label: `moyenne des bouteilles supprimer / utilisateur`,
         data: [180, 240, 360, 190, 320],
         borderColor: 'rgba(127,29,29,1)',
         backgroundColor: 'rgba(127,29,29,0.5)',
