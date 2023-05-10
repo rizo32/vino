069f56db87b6eac4c3cb6dd1abfd312b5d05bd71
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-10 12:50:37 -0400

    ajout d'onglet et modificatiosn style 2match

--- react/src/views/Admin.jsx
+++ react/src/views/Admin.jsx
@@ -8,6 +8,9 @@ const Admin = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [activeTab, setActiveTab] = useState("users");
+
+  
 
   useEffect(() => {
     axios
@@ -54,70 +57,69 @@ const Admin = () => {
   
 
 
-
-
-
-
-
-
   return (
     <div className="pt-4 container mx-auto">
-      <h1 className="text-2xl flex justify-center font-bold mb-4">Modifications utilisateurs</h1>
       <div className="flex justify-center mb-4">
-        <input
-          type="text"
-          placeholder="Rechercher par Prénom"
-          value={searchTerm}
-          onChange={handleSearch}
-          className="px-4 py-2 border border-gray-300 rounded-lg"
-        />
+        <button
+          className={`mr-4 ${activeTab === "users" ? "font-bold" : ""}`}
+          onClick={() => setActiveTab("users")}
+        >
+          Users
+        </button>
+        <button
+          className={`ml-4 ${activeTab === "stats" ? "font-bold" : ""}`}
+          onClick={() => setActiveTab("stats")}
+        >
+          Stats
+        </button>
       </div>
-      <div className="grid grid-cols-2 gap-4">
-        {filteredUsers.length > 0 ? (
-          filteredUsers.map((user) => (
-            <div key={user.id} className="bg-gray-200 p-4 rounded-lg shadow flex items-center justify-between">
-              <div>
-                <p className="text-lg font-bold">{user.first_name}</p>
-                <p>{user.email}</p>
-              </div>
-              <button className="px-4 py-2 bg-red-900 text-white rounded-lg" onClick={() => openModal(user)}>
-                Modifications
-              </button>
-            </div>
-          ))
-        ) : (
-          <p>No users found.</p>
-        )}
-      </div>
-      {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="bg-white p-8 max-w-md mx-auto rounded-lg shadow-lg">
-            <h2 className="text-xl font-bold mb-4">Modifications utilisateur</h2>
-            {selectedUser && (
-              <div>
-                <p className="text-lg">User: {selectedUser.first_name}</p>
-                <p>Email: {selectedUser.email}</p>
-                <label htmlFor="userTypes">Type d'utilisateurs</label>
-                <select name="userTypes" id="userTypes" className="px-4 py-2 border border-gray-300 rounded-lg">
-                  <option value="1">Admin</option>
-                  <option value="2">Employées</option>
-                  <option value="3">Utilisateurs</option>
-                  <option value="4">Bannir</option>
-                </select>
-              </div>
+      {activeTab === "users" && (
+        <div className="px-4">
+          <h1 className="text-2xl font-bold mb-4">Modifications utilisateurs</h1>
+          <div className="flex justify-center mb-4">
+            <input
+              type="text"
+              placeholder="Rechercher par Prénom"
+              value={searchTerm}
+              onChange={handleSearch}
+              className="px-4 py-2 border border-gray-300 rounded-lg w-full sm:w-auto"
+            />
+          </div>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {filteredUsers.length > 0 ? (
+              filteredUsers.map((user) => (
+                <div key={user.id} className="bg-gray-200 p-4 rounded-lg shadow">
+                  <div>
+                    <p className="text-lg font-bold">{user.first_name}</p>
+                    <p>{user.email}</p>
+                  </div>
+                  <button className="px-4 py-2 bg-red-900 text-white rounded-lg mt-4" onClick={() => openModal(user)}>
+                    Modifications
+                  </button>
+                </div>
+              ))
+            ) : (
+              <p>No users found.</p>
             )}
-            <button className="px-4 py-2 bg-red-900 text-white rounded-lg mt-4" onClick={closeModal}>
-              Fermer
-            </button>
-            <button className="px-4 py-2 bg-red-900 text-white rounded-lg mt-4" onClick={updateUser}>
-                Sauvegarder
-            </button>
-         </div>
+          </div>
+          {showModal && (
+            <div className="fixed inset-0 flex items-center justify-center z-50">
+              <div className="bg-white p-8 max-w-md mx-auto rounded-lg shadow-lg">
+                {/* Existing code for the modal */}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+      {activeTab === "stats" && (
+        <div>
+          {/* Add your stats content here */}
         </div>
       )}
     </div>
   );
-};
+  
+    };
 
 export default Admin;
 
