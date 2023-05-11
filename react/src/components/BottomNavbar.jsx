037782092fee23ef8c1a1e17c607d88ec7d1ba47
rizo32 <g.rizzoli@hotmail.ca>
2023-05-11 14:35:04 -0400

    homogénéisation épaisseur icones

--- react/src/components/BottomNavbar.jsx
+++ react/src/components/BottomNavbar.jsx
@@ -36,7 +36,7 @@ const BottomNavbar = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        strokeWidth={2}
+                        strokeWidth={1.5}
                         stroke="currentColor"
                         className="w-12 h-12 p-2 bg-red-900 text-white rounded-full shadow-shadow-md"
                     >
@@ -59,7 +59,7 @@ const BottomNavbar = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="black"
                         viewBox="0 0 24 24"
-                        strokeWidth={2}
+                        strokeWidth={1.5}
                         stroke="currentColor"
                         className="w-8 h-8 cursor-pointer"
                     >
@@ -71,29 +71,6 @@ const BottomNavbar = () => {
                     </svg>
                 </div>
             </NavLink>
-            {/* <NavLink
-    to={`/users/${user.id}`}
-    className={({ isActive, isPending }) =>
-        isPending ? "pending" : isActive ? "active" : ""
-    }
->
-    <div className="flex flex-col items-center">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="000000"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-8 h-10"
-        >
-            <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-        </svg>
-    </div>
-</NavLink> */}
         </div>
     );
 };
