37dd5895509cdc44948cded2dd0ca02ccd76c137
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-11 15:44:02 -0400

    implementation des methodes a utiliser

--- app/Http/Controllers/Api/StatsController.php
+++ app/Http/Controllers/Api/StatsController.php
@@ -10,5 +10,40 @@ use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    //
+    public function getStats()
+    {
+        $numberOfUsers = $this->getNumberOfUsers();
+        $avgCellarTotalWorth = $this->getAvgCellarTotalWorth();
+        $totalCellarWorth = $this->getTotalCellarWorth();
+        $wineOfTheMoment = $this->getWineOfTheMoment();
+
+        return response()->json([
+            'numberOfUsers' => $numberOfUsers,
+            'avgCellarTotalWorth' => $avgCellarTotalWorth,
+            'totalCellarWorth' => $totalCellarWorth,
+            'wineOfTheMoment' => $wineOfTheMoment,
+        ]);
+    }
+
+    private function getNumberOfUsers()
+    {
+    
+       
+    }
+
+    private function getAvgCellarTotalWorth()
+    {
+
+    }
+
+    private function getTotalCellarWorth()
+    {
+
+    }
+
+    private function getWineOfTheMoment()
+    {
+        
+    }
 }
+
