b99a3638b1e99e301ee5f912edd37a42b9a954d8
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-11 15:53:58 -0400

    Modifications d'une erreur, lors de la creation

--- app/Http/Controllers/Api/StatsController.php
+++ app/Http/Controllers/Api/StatsController.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller; 
 use App\Models\Bottle;
 use App\Models\User;
 use App\Models\Cellar;
