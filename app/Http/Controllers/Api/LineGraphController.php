f459f182aad0e3da5a7f4a0d39e2913f405e0d04
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-11 18:58:54 -0400

    Ajout structure de base pour lineGraph

---
+++ app/Http/Controllers/Api/LineGraphController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller; 
+use App\Models\Bottle;
+use App\Models\User;
+use App\Models\CellarHasBottle;
+use App\Models\Cellar;
+use App\Models\Country;
+
+use Illuminate\Http\Request;
+
+class LineGraphController extends Controller
+{
+    
+}
