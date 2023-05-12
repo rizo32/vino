08f97fa159f2f6d4af3a7272df92894256b8d479
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-12 07:27:15 -0400

    Ajout donné supplémentaire  ajustement chromatique

--- react/src/components/Stats/DataSetB.jsx
+++ react/src/components/Stats/DataSetB.jsx
@@ -1,8 +1,8 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import axios from "axios";
-import  ThirdDataSet  from "./DataSetC";
+import ThirdDataSet from "./DataSetC";
 const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/piestats`;
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -15,7 +15,7 @@ const SecondDataSet = () => {
       try {
         const response = await axios.get(baseURL);
         setWineData(response.data);
-        console.log(response.data)
+        console.log(response.data);
       } catch (error) {
         console.error("Error fetching wine data:", error);
       }
@@ -25,13 +25,29 @@ const SecondDataSet = () => {
   }, []);
 
   const data = {
-    labels: ["Vin rouge", "Vin blanc"],
+    labels: ["Vin rouge", "Vin blanc", "Vin rosé", "Autres"],
     datasets: [
       {
         label: "nombre de bouteilles",
-        data: [wineData.redWineCount, wineData.whiteWineCount],
-        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(255, 206, 86, 0.2)"],
-        borderColor: ["rgba(255, 99, 132, 1)", "rgba(255, 206, 86, 1)"],
+        data: [
+          wineData.redWineCount,
+          wineData.whiteWineCount,
+          wineData.pinkWineCount,
+          wineData.otherWineCount,
+        ],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)", // Red Wine
+          "rgba(255, 206, 86, 0.2)", // White Wine
+          "rgba(255, 159, 243, 0.2)", // Pink Wine
+          "rgba(153, 102, 255, 0.2)", // Other Wine
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)", // idem
+          "rgba(255, 206, 86, 1)",
+          "rgba(255, 159, 243, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+
         borderWidth: 1,
       },
     ],
@@ -50,19 +66,16 @@ const SecondDataSet = () => {
 
   return (
     <div className="flex flex-col items-center w-full">
-      <h2 className="text-small mb-4">
-        Statistiques celliers
-      </h2>
+      <h2 className="text-small mb-4">Statistiques celliers</h2>
       <div className="grid grid-cols-2 gap-4 w-full">
         <div className="flex justify-center w-full h-56 md:h-64 lg:h-96">
           <Pie data={data} options={options} />
         </div>
         <div className="flex justify-center w-full h-56 md:h-64 lg:h-96">
-          <ThirdDataSet/>
+          <ThirdDataSet />
         </div>
       </div>
     </div>
   );
-
-}
+};
 export default SecondDataSet;
