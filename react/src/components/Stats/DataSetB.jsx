d784e4488ad8101ddaaabe2120c1dcd85c2d1704
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-12 06:45:46 -0400

    Modifications pour fetch dynamique des donnée pie1

--- react/src/components/Stats/DataSetB.jsx
+++ react/src/components/Stats/DataSetB.jsx
@@ -1,36 +1,52 @@
-import React from "react";
+import React,{ useEffect, useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
+import axios from "axios";
 import  ThirdDataSet  from "./DataSetC";
-import wineAvg from "./WineTypeAvg"
+const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/piestats`;
 
 ChartJS.register(ArcElement, Tooltip, Legend);
+
 const SecondDataSet = () => {
- const data = {
-  labels: ["Vin rouge", "Vin blanc"],
-  datasets: [
-    {
-      label: "nombre de bouteilles",
-      data: [wineAvg['red'], wineAvg['white']],
-      backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(255, 206, 86, 0.2)"],
-      borderColor: ["rgba(255, 99, 132, 1)", "rgba(255, 206, 86, 1)"],
-      borderWidth: 1,
-    },
-  ],
-};
+  const [wineData, setWineData] = useState({ red: 0, white: 0 });
 
-const options = {
-  plugins: {
-    title: {
-      display: true,
-      text: "Total des bouteilles par type",
-    },
-    legend: {
-      position: "chartArea",
-    },
-  },
-};
+  useEffect(() => {
+    const fetchWineData = async () => {
+      try {
+        const response = await axios.get(baseURL);
+        setWineData(response.data);
+        console.log(response.data)
+      } catch (error) {
+        console.error("Error fetching wine data:", error);
+      }
+    };
 
+    fetchWineData();
+  }, []);
+
+  const data = {
+    labels: ["Vin rouge", "Vin blanc"],
+    datasets: [
+      {
+        label: "nombre de bouteilles",
+        data: [wineData.redWineCount, wineData.whiteWineCount],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(255, 206, 86, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(255, 206, 86, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Total des bouteilles par type",
+      },
+      legend: {
+        position: "chartArea",
+      },
+    },
+  };
 
   return (
     <div className="flex flex-col items-center w-full">
