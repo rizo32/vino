41ab21a98f8f6f62dd2c92333e41a3f6375e57e2
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-12 08:10:03 -0400

    Ajout de logique pour attribution dynamique donne

--- react/src/components/Stats/DataSetC.jsx
+++ react/src/components/Stats/DataSetC.jsx
@@ -5,13 +5,17 @@ import { Pie } from 'react-chartjs-2';
 const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/topWineStats`;
 
 const ThirdDataSet = () => {
-  const [topWineData, setTopWineData] = useState(null);
+  const [topWineData, setTopWineData] = useState({ labels: [], data: [] }); /* doit mapper etant donner que les labels sont changeeant dépendemment du score  */
+
 
   useEffect(() => {
     const fetchWineData = async () => {
       try {
         const response = await axios.get(baseURL);
-        setTopWineData(response.data);
+        const labels = response.data.topWineStats.map((item) => item.name);
+        const data = response.data.topWineStats.map((item) => parseInt(item.bottles_count));
+        setTopWineData({ labels, data });
+        
         console.log(response.data);
       } catch (error) {
         console.error("Error fetching wine data:", error);
@@ -22,17 +26,18 @@ const ThirdDataSet = () => {
   }, []);
 
   const data2 = {
-    labels: ['Italie', 'Portugal', 'France', 'Espagne', 'Chili'],
+    labels: topWineData.labels,
     datasets: [
       {
         label: 'nombre de bouteilles',
-        data: [95, 53, 22, 100, 111],
+        data: topWineData.data,
         backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
         borderColor: ['rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)', 'rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
         borderWidth: 1,
       },
     ],
   };
+  
   const options2 = {
     plugins: {
       title: {
