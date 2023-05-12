d4e5db496523c613cd84a4bc2ce71b4cd53ca588
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-12 07:20:39 -0400

    mis a jour de getWineStats --vraiVersion

--- app/Http/Controllers/Api/PieGraphController.php
+++ app/Http/Controllers/Api/PieGraphController.php
@@ -15,14 +15,44 @@ class PieGraphController extends Controller
 {
     public function getWineStats()
     {
-       /* data simple pour test -> manque logique */
-        $redWineCount = Bottle::where('type_id', '1')->count();
-        $whiteWineCount = Bottle::where('type_id', '2')->count();
-
-     
+        // initialise les counters
+        $redWineCount = 0;
+        $whiteWineCount = 0;
+        $pinkWineCount = 0;
+        $otherWineCount = 0;
+    
+        /* loop a travers to les cellars */
+        $cellars = Cellar::all();
+        foreach ($cellars as $cellar) {
+            // Prend tou les bouteilles associés au cellier
+            $cellarHasBottles = CellarHasBottle::where('cellar_id', $cellar->id)->get();
+    
+            // Traverse tout les donné associés au bouteilles
+            foreach ($cellarHasBottles as $cellarHasBottle) {
+                //Trouve bouteille et quantité
+                $bottle = Bottle::find($cellarHasBottle->bottle_id);
+                $quantity = $cellarHasBottle->quantity;
+    
+                //mis a jour du counter selon le type de bouteille
+                if ($bottle->type_id == 1) {
+                    $redWineCount += $quantity;
+                } elseif ($bottle->type_id == 2) {
+                    $whiteWineCount += $quantity;
+                } elseif ($bottle->type_id == 3){
+                    $pinkWineCount += $quantity;
+                } elseif ($bottle->type_id == 4){
+                    $otherWineCount += $quantity;
+                } 
+                
+            }
+        }
+    
+        
         return response()->json([
             'redWineCount' => $redWineCount,
             'whiteWineCount' => $whiteWineCount,
+            'pinkWineCount' => $pinkWineCount,
+            'otherWineCount' => $otherWineCount,
         ]);
     }
 }
