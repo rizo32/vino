7240b7cb69b00d87eda4048c9886f59fcc10dcc8
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-13 19:58:10 -0400

    Inversion de l'ordre des btns --YG

--- react/src/views/Admin.jsx
+++ react/src/views/Admin.jsx
@@ -57,25 +57,26 @@ const Admin = () => {
   };
 
   return (
-    <div className="pt-4 container mx-auto">
-      <div className="flex justify-center mb-4">
-        <button
-          className={`mr-4 px-4 py-2 rounded-lg border ${
-            activeTab === "users" ? " bg-red-900 text-white" : "border-gray-300"
-          }`}
-          onClick={() => setActiveTab("users")}
-        >
-          Users
-        </button>
-        <button
-          className={`ml-4 px-4 py-2 rounded-lg border ${
-            activeTab === "stats" ?  "bg-red-900 text-white" : "border-gray-300"
-          }`}
-          onClick={() => setActiveTab("stats")}
-        >
-          Stats
-        </button>
-      </div>
+   <div className="pt-4 container mx-auto">
+  <div className="flex justify-center mb-4">
+    <button
+      className={`mr-4 px-4 py-2 rounded-lg border ${
+        activeTab === "stats" ? "bg-red-900 text-white" : "border-gray-300"
+      }`}
+      onClick={() => setActiveTab("stats")}
+    >
+      Stats
+    </button>
+    <button
+      className={`ml-4 px-4 py-2 rounded-lg border ${
+        activeTab === "users" ? "bg-red-900 text-white" : "border-gray-300"
+      }`}
+      onClick={() => setActiveTab("users")}
+    >
+      Users
+    </button>
+  </div>
+
       {activeTab === "users" && (
         <div className="px-4">
           <div className="flex justify-center mb-4">
