9973ff9e14d068b2c2a14eab7866fabfe425cc4f
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-16 09:49:56 -0400

    ajout de commentaire TopWineCharts

--- react/src/components/Stats/TopWinePieChart.jsx
+++ react/src/components/Stats/TopWinePieChart.jsx
@@ -1,23 +1,25 @@
+/* imporantations des composantes et modules */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Pie } from 'react-chartjs-2';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 
-
+/* déclarations du baseURL */
 const baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/topWineStats`;
 
 const TopWinePieChart = () => {
+  /* déclarations des states */
   const [topWineData, setTopWineData] = useState({ labels: [], data: [] }); /* doit mapper etant donner que les labels sont changeeant dépendemment du score  */
 
 
   useEffect(() => {
     const fetchWineData = async () => {
       try {
-        const response = await axios.get(baseURL);
-        const labels = response.data.topWineStats.map((item) => item.name);
-        const data = response.data.topWineStats.map((item) => parseInt(item.bottles_count));
-        setTopWineData({ labels, data });
+        const response = await axios.get(baseURL); /* requête axios pour récupérer les données de l'API */
+        const labels = response.data.topWineStats.map((item) => item.name);   /* déclaration des labels recceuillis */
+        const data = response.data.topWineStats.map((item) => parseInt(item.bottles_count)); /* déclaration des données recceuillis */
+        setTopWineData({ labels, data }); /* déclaration de topWineData avec les labels and data */
         
        
       } catch (error) {
@@ -29,6 +31,7 @@ const TopWinePieChart = () => {
   }, []);
 
   const data2 = {
+    /* ajout des labels et données au graphique */
     labels: topWineData.labels,
     datasets: [
       {
@@ -42,6 +45,7 @@ const TopWinePieChart = () => {
   };
   
   const options2 = {
+    /* Déclaration des options au niveau esthétiques  et labels pour se graphique */
     plugins: {
       title: {
         display: true,
@@ -66,11 +70,12 @@ const TopWinePieChart = () => {
           weight: 'bold' 
         },
         padding: 2, 
-        formatter: function (value, context) {
+        formatter: function (value, context) { /* ajout du % de la partie a l'intérieur de celle-ci */
           const total = context.dataset.data.reduce((a, b) => a + b, 0);
           const percentage = ((value / total) * 100).toFixed(2);
           return percentage + '%';
         },
+         /* placement et style du % */
         anchor: "end", 
         align: "end", 
         offset: -25, 
@@ -82,7 +87,7 @@ const TopWinePieChart = () => {
 
   return (
     <div className="w-full h-full">
-      <Pie data={data2} options={options2} plugins={[ChartDataLabels]} />
+      <Pie data={data2} options={options2} plugins={[ChartDataLabels]} /> {/* ajout des données et options au graphique */}
     </div>
   );
 };
