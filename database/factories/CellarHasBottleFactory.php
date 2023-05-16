24a2684f89d297d5f574bce56f03574dc85ada8b
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-16 10:05:35 -0400

    ajout de commentaire CellarHasBottleFactory

--- database/factories/CellarHasBottleFactory.php
+++ database/factories/CellarHasBottleFactory.php
@@ -15,8 +15,8 @@ class CellarHasBottleFactory extends Factory
         return [
             'cellar_id' => null, //vont etre ajouter lors du seeding
             'bottle_id' => null, // vont etre ajouter lors du seeding
-            'quantity' => $this->faker->numberBetween(1, 10),
-            'created_at' => $this->faker->dateTimeBetween('-6 months', 'now'),
+            'quantity' => $this->faker->numberBetween(1, 10), /* aleatoire entre 1 et 10 bouteilles  */
+            'created_at' => $this->faker->dateTimeBetween('-6 months', 'now'), /*  date de creation aleatoire entre aujourd'hui et moins 6 mois ( pour simuler 6 mois d'interactions ) */
         ];
     }
 }
