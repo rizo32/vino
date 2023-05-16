91f496fd7f71a6549354ae61c4d21cef64311497
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-15 20:05:08 -0400

    logique cellarhasbottle seeding

--- database/seeders/CellarHasBottleSeeder.php
+++ database/seeders/CellarHasBottleSeeder.php
@@ -2,18 +2,38 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Cellar;
+use App\Models\Bottle;
+use App\Models\CellarHasBottle;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
 
 class CellarHasBottleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        
+        $bottleIds = Bottle::pluck('id')->toArray();
+
+        Cellar::all()->each(function ($cellar) use ($bottleIds) {
+           
+            $numBottles = random_int(15, 100);
+
+            for ($i = 0; $i < $numBottles; $i++) {
+                
+                $bottle_id = Arr::random($bottleIds);
+
+                
+                $created_at = Carbon::now()->subMonths(6)->addSeconds(random_int(0, 6 * 30 * 24 * 60 * 60));
+
+                CellarHasBottle::create([
+                    'cellar_id' => $cellar->id,
+                    'bottle_id' => $bottle_id,
+                    'quantity' => random_int(1, 10),
+                    'created_at' => $created_at,
+                ]);
+            }
+        });
     }
 }
