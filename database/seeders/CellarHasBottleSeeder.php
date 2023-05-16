b289e5a5493916d402939665c28b555c3da72521
YG Shinigami <98998507+ShinigamiXYZ@users.noreply.github.com>
2023-05-15 20:01:04 -0400

    creation de seeders&factory pouur celllarhasbottle

---
+++ database/seeders/CellarHasBottleSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CellarHasBottleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
